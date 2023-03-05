6581ba41cb66365dd0ba89264ff83ac97d50fd74
Dostonlv <iamitachiuchixa@gmail.com>
2023-03-05 11:42:36 +0500

    Factorial

--- 2-lesson/FizzBuzz/FizzBuzz.go
+++ 2-lesson/FizzBuzz/FizzBuzz.go
@@ -1,5 +1,13 @@
-package main
+package FizzBuzz
 
-func FizzBuzz() {
-	
+import "fmt"
+
+func FizzBuzz(a int) {
+	if a%15 == 0 {
+		fmt.Println("FizzBuzz")
+	} else if a%5 == 0 {
+		fmt.Println("Fizz")
+	} else if a%3 == 0 {
+		fmt.Println("Buzz")
+	}
 }
--- 2-lesson/main.go
+++ 2-lesson/main.go
@@ -1,14 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"lesson2/Factorial"
+)
 
 func main() {
 	print("Sonni kiriting: ")
 	var a int
 	fmt.Scan(&a)
-	sum := 0
-	for i := 0; i <= a; i++ {
-		sum += i
-	}
-	fmt.Println(sum)
+	//sum := 0
+	//for i := 0; i <= a; i++ {
+	//	if i%2 == 0 {
+	//		sum += i
+	//	}
+	//}
+	//fmt.Println(sum)
+	//FizzBuzz.FizzBuzz(a)
+	f := Factorial.Factorial(a)
+	fmt.Println(f)
 }
